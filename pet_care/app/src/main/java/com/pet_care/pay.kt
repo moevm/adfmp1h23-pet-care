29c50c3325d3746520ef6b272d9053cef8147e6e
pautowa <iulchi.pautowa@yandex.ru>
2023-03-09 18:43:51 +0300

    fixing summ when buying care

--- pet_care/app/src/main/java/com/pet_care/pay.kt
+++ pet_care/app/src/main/java/com/pet_care/pay.kt
@@ -1,12 +1,10 @@
 package com.pet_care
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import com.pet_care.R
+import android.content.Intent
 
 
 class pay : AppCompatActivity() {
@@ -14,12 +12,22 @@ class pay : AppCompatActivity() {
     private lateinit var button_pay: Button
     private lateinit var summ: EditText
 
+    companion object{
+        const val price = "price"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pay)
 
         button_pay = findViewById(R.id.button_pay)
         summ = findViewById(R.id.summ)
+
+        val price: String = intent.getStringExtra(price)?: "null"
+        if(price != "null"){
+            summ.setText(price)
+            summ.isFocusable = false
+        }
     }
 
 //    fun clickPay(view: View) {
